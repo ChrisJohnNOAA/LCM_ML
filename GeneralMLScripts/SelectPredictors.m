98d49bf60f6b4745432bba6bdde8ed318eb8e15b
Chris John <Chris.P.John@gmail.com>
2022-09-28 09:44:27 -0700

    Try using sequential feature selection to improve predictions for some variables
    This is very slow so only use it where it seems necessary.

---
+++ GeneralMLScripts/SelectPredictors.m
@@ -0,0 +1,53 @@
+function [predictors, scoreStruct] = SelectPredictors(varName, data, predictors, target, maxPredictors, scoreStruct, indepthSelection, kernelfcn)
+    if (indepthSelection)
+        [predictors, scoreStruct] = SequentialSelection(varName, data, predictors, target, maxPredictors, scoreStruct, kernelfcn);
+    else
+        [predictors, scoreStruct] = QuickSelection(varName, data, predictors, target, maxPredictors, scoreStruct);
+    end
+
+end
+
+function [predictors, scoreStruct] = QuickSelection(varName, data, predictors, target, maxPredictors, scoreStruct)
+    % Check for correlation p < 0.05
+    [r, p] = corr(table2array(data(:, predictors)), ...
+        target, 'rows','complete');
+
+    scoreStruct.(varName+"_corr") = table(predictors', r, p);
+    scoreStruct.(varName+"_corr") = sortrows(scoreStruct.(varName+"_corr"),'r','descend');
+
+    predictors = predictors(p<0.05);
+
+    % Check for fscore > 100
+    [featureIndex, featureScore] = fsrftest(...
+        data(:, predictors), ...
+        target);
+
+    scoreStruct.(varName+"_f") = table(predictors(featureIndex)', featureScore(featureIndex)');
+    predictors = predictors(featureScore > 400);
+    
+    % Use mrmr to select top X from remaining (15?)
+    [featureIndex, featureScore] = fsrmrmr(...
+        data(:, predictors), ...
+        target);
+
+    scoreStruct.(varName+"_mrmr") = table(predictors(featureIndex)', featureScore(featureIndex)');
+    predictors = predictors(featureIndex);
+    predictors = predictors(1:min(maxPredictors,length(predictors)));
+end
+
+function [predictors, scoreStruct] = SequentialSelection(varName, data, predictors, target, maxPredictors, scoreStruct, kernelfcn)
+
+c = cvpartition(target,'k',10);
+opts = statset('Display','iter','UseParallel',true);
+fun = @(XTrain,YTrain,XTest,YTest)loss(fitrgp(...
+    XTrain, ...
+    YTrain, ...
+    'BasisFunction', 'constant', ...
+    'Standardize', true, ...
+    'PredictMethod','exact', ... % It defaults to 'bcd' if training data > 10,000 and I'm using 13,536 rows, but bcd seems to get stuck training
+    'KernelFunction', kernelfcn),XTest,YTest);
+
+[fs,history] = sequentialfs(fun,table2array(data(:, predictors)),target,'cv',c,'options',opts);
+scoreStruct.(varName+"_history") = history;
+predictors = predictors(fs);
+end
